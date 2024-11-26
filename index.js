f6000205e19ca03dee122a7c10dc88a8cd0c8574
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-11-27 02:03:56 +0600

    delete

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/coffee/:id" , async (req,res) => {
+      const id = req.params.id;
+
+      const result = await coffeeCollecitn.deleteOne({_id : new ObjectId(id)})
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
